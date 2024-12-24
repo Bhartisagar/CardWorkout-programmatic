c889bc2daf80385cbb2d88105fafcaebf4154be2
Bhartisagar <53816596+Bhartisagar@users.noreply.github.com>
2024-12-24 19:51:47 +0530

    test files

---
+++ CardWorkout-programmaticTests/CardWorkout_programmaticTests.swift
@@ -0,0 +1,17 @@
+//
+//  CardWorkout_programmaticTests.swift
+//  CardWorkout-programmaticTests
+//
+//  Created by Bharti Sagar on 24/12/24.
+//
+
+import Testing
+@testable import CardWorkout_programmatic
+
+struct CardWorkout_programmaticTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
